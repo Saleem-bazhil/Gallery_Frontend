560ea6a18a488be785618312eaf2d995235a51ca
saleem-bazhil <ab588538@gmail.com>
2025-10-19 12:47:39 +0530

    update

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+export const BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: BASE_URL,
